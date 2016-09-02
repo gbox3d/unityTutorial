b14d6d3feadeb23b4bd43d56a36211f26066eec6
도플광어 <gbox3d@gmail.com>
2016-09-02 19:15:15 +0900

    rx sample

---
+++ rx_sample/Assets/rxex2_trigger_basic/rxex2_axis_controll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class rxex2_axis_controll : MonoBehaviour {
+
+	void Start () {
+
+		gameObject.AddComponent<ObservableUpdateTrigger> ()
+			.UpdateAsObservable ()
+			.Select (_=> new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical" ) ))
+			.Subscribe (
+				(evt)=> {
+					transform.Translate(evt.x * Time.deltaTime * 10,0,evt.y * Time.deltaTime * 10);
+				}
+			).AddTo(gameObject);
+
+
+
+
+	}
+}
---
+++ rx_sample/Assets/rxex3_trigger_repeat/rxex3_pick_and_roll.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class rxex3_pick_and_roll : MonoBehaviour {
+
+	private float _rotationSpeed = 500.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		var obs = gameObject.AddComponent<ObservableUpdateTrigger> ();
+		obs.UpdateAsObservable()                      
+			.SkipUntil( obs.OnMouseDownAsObservable() ) 
+			.Select(
+				_ => new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y")))
+			.TakeUntil( obs.OnMouseUpAsObservable())   
+			.Repeat()                             
+			.Subscribe(
+				(move) => {
+					transform.rotation =
+						Quaternion.AngleAxis(move.y * _rotationSpeed * Time.deltaTime, Vector3.right) *
+						Quaternion.AngleAxis(-move.x * _rotationSpeed * Time.deltaTime, Vector3.up) *
+						transform.rotation;
+				}
+			);
+
+		obs.UpdateAsObservable ()
+			.Select (_ => Input.GetButtonDown ("Fire1"))
+			.Where(x=>x)
+			.Subscribe (
+				(x)=> {
+					gameObject.GetComponent<Renderer>().material.color = Color.red;
+				}
+			);
+		
+		
+	}
+}
