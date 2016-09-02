287c5c82d98352567c5363a79f6e32c69298f27c
도플광어 <gbox3d@gmail.com>
2016-09-02 15:47:57 +0900

    ui sample

---
+++ ui_sample/Assets/exam6_uirx_input/ex4_inputfiled.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UniRx;
+
+public class ex4_inputfiled : MonoBehaviour {
+
+	[SerializeField] private Button btnTest;
+	[SerializeField] private InputField inpfdTest;
+	[SerializeField] private Text textOutResult;
+
+	// Use this for initialization
+	void Start () {
+
+		btnTest.onClick.AsObservable ()
+			.Subscribe (_ => {
+				textOutResult.text = inpfdTest.text;
+
+				
+				
+			}
+		);
+	
+	}
+
+}
