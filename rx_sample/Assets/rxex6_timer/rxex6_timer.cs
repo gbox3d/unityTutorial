6aee1e93993cb9a4bb4aee6bf158eaf1192bebda
도플광어 <gbox3d@gmail.com>
2016-09-09 13:52:27 +0900

    rx sample

---
+++ rx_sample/Assets/rxex6_timer/rxex6_timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using System;
+
+public class rxex6_timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Observable.Interval (TimeSpan.FromMilliseconds (1000))
+			.Subscribe (
+				_=> {
+					Debug.Log("tick :" + Time.realtimeSinceStartup);
+				}
+			);
+
+		//Observable.Timer
+
+		Observable.Timer (TimeSpan.FromMilliseconds (5000))
+			.Subscribe (
+				_=> {
+					Debug.Log("timer :" + Time.realtimeSinceStartup);
+				}
+		);
+		
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
