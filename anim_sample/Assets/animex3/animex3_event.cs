aacfcda2d321e3befae85df7ab08ccf386032cda
도플광어 <gbox3d@gmail.com>
2016-09-12 14:34:47 +0900

    animation animator sample

---
+++ anim_sample/Assets/animex3/animex3_event.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animex3_event : MonoBehaviour {
+
+	public void setRed() {
+
+		gameObject.GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+	public void setWhite() {
+		gameObject.GetComponent<Renderer> ().material.color = Color.white;
+	}
+
+}
