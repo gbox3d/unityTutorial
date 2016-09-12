874cc3d89b1af9489b4eea8f86fc655e783dbce6
도플광어 <gbox3d@gmail.com>
2016-09-12 11:01:34 +0900

    example 9 add

---
+++ basic/Assets/script/ex7_class/ex7_test1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ex7_test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1")) {
+
+			ex7_test2 comp = gameObject.GetComponent<ex7_test2>() as ex7_test2;
+
+			comp.val_test1 = "unity";
+			comp.call_test("hello");
+
+			ex7_test3.call_test ("hi~");
+
+			//Debug.Log("myclass public vale : " + comp.call_test1() );
+			//scene7_myclass.test();
+		}
+	
+	}
+}
---
+++ basic/Assets/script/ex7_class/ex7_test3.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ex7_test3 : MonoBehaviour {
+
+	static public float public_vartest = 3.14f; 
+
+	static public void call_test( string param )
+	{
+		Debug.Log( public_vartest );
+		Debug.Log( param );
+	}
+}
