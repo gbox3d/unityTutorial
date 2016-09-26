17c96dc43a2b5c6e27b3d2fd2573bff435fe5d8a
도플광어 <gbox3d@gmail.com>
2016-09-26 21:31:30 +0900

    ui rx instance sample

---
+++ ui_sample/Assets/exam10_uirx_drag/exam10_uirx_drag.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+using UniRx;
+using UniRx.Triggers;
+
+public class exam10_uirx_drag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		//Debug.Log (gameObject.GetComponent<RectTransform>().rect.ToString());
+		//Debug.Log (gameObject.GetComponent<RectTransform>().position.ToString());
+		//Debug.Log (rect_area.ToString());
+
+		int FSM = 0;
+
+		gameObject.UpdateAsObservable ()
+			.Select (_ => Input.mousePosition)
+			.Where((pos)=> {
+				Vector3 pos_tran = gameObject.GetComponent<RectTransform>().position;
+				Vector2 mouse_pos = new Vector2(
+					pos.x - pos_tran.x,
+					pos.y - pos_tran.y);
+
+
+				if(gameObject.GetComponent<RectTransform>().rect.Contains(mouse_pos)) {
+
+					if(FSM == 1) return false;
+					else {
+						FSM = 1;
+					}
+					
+					return true;
+				}
+
+				return false;
+				
+			})
+			.Subscribe ((pos) => {
+				Debug.Log( Mathf.RoundToInt( pos.x) + " , " +  Mathf.RoundToInt( pos.y) );
+				gameObject.GetComponent<Image>().color = Color.red;
+
+				
+		});
+
+		gameObject.UpdateAsObservable ()
+			.Select (_ => Input.mousePosition)
+			.Where((pos)=> {
+				Vector3 pos_tran = gameObject.GetComponent<RectTransform>().position;
+				Vector2 mouse_pos = new Vector2(
+					pos.x - pos_tran.x,
+					pos.y - pos_tran.y);
+
+
+				if(gameObject.GetComponent<RectTransform>().rect.Contains(mouse_pos)) {
+
+					return false;
+				}
+
+				if(FSM == 0) return false;
+				else {
+					FSM = 0;
+				}
+
+				return true;
+
+			})
+			.Subscribe ((pos) => {
+				Debug.Log( Mathf.RoundToInt( pos.x) + " , " +  Mathf.RoundToInt( pos.y) );
+				gameObject.GetComponent<Image>().color = Color.green;
+
+
+			});
+
+
+
+
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ui_sample/Assets/exam9_uirx_instantiate/exam9_main.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UniRx;
+using UniRx.Triggers;
+
+using UnityEngine.UI;
+
+
+
+public class exam9_main : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+
+		//http://answers.unity3d.com/questions/674127/how-to-find-a-prefab-via-script.html
+		GameObject prefeb_test = Resources.Load ("testUI",typeof(GameObject)) as GameObject;
+		Button btn_Test = GameObject.Find ("Canvas/Button_Instantiate").GetComponent<Button> ();
+		GameObject root_canvas = GameObject.Find ("Canvas");
+
+		btn_Test.OnClickAsObservable ()
+			.Subscribe (_ => {
+				Debug.Log("click");
+				GameObject obj = (GameObject)Instantiate (prefeb_test,root_canvas.transform );
+				obj.GetComponent<RectTransform> ().localPosition = new Vector3 (Random.Range(-500.0f, 500.0f),Random.Range(-200.0f, 200.0f), 0);
+		});
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
