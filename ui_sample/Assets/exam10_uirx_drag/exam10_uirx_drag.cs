418af61abadfa995e758e284a745c8564f41a493
도플광어 <gbox3d@gmail.com>
2016-09-26 22:26:20 +0900

    exam10

--- ui_sample/Assets/exam10_uirx_drag/exam10_uirx_drag.cs
+++ ui_sample/Assets/exam10_uirx_drag/exam10_uirx_drag.cs
@@ -11,37 +11,25 @@ public class exam10_uirx_drag : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		int FSM = 0;
+
 
 		gameObject.UpdateAsObservable ()
-			.Select (_ => Input.mousePosition)
-			.Where((pos)=> {
+			.Select (_ => {
+				Vector3 pos = Input.mousePosition;
 				Vector3 pos_tran = gameObject.GetComponent<RectTransform>().position;
-				Vector2 mouse_pos = new Vector2(
-					pos.x - pos_tran.x,
-					pos.y - pos_tran.y);
-
-
-				if(gameObject.GetComponent<RectTransform>().rect.Contains(mouse_pos)) {
-
-					if(FSM == 1) return false;
-					else {
-						FSM = 1;
-					}
-					
-					return true;
-				}
-
-				return false;
-				
+				return new Vector2(pos.x - pos_tran.x,pos.y - pos_tran.y);
 			})
-			.Subscribe ((pos) => {
-				Debug.Log( Mathf.RoundToInt( pos.x) + " , " +  Mathf.RoundToInt( pos.y) );
-				gameObject.GetComponent<Image>().color = Color.red;
-
+			.DistinctUntilChanged() // remove samethings
+			.Select(pos=>gameObject.GetComponent<RectTransform>().rect.Contains(pos))
+			.Subscribe ((hover) => {
+				//Debug.Log( Mathf.RoundToInt( pos.x) + " , " +  Mathf.RoundToInt( pos.y) );
+				if(hover == true) gameObject.GetComponent<Image>().color = Color.red;
+				else gameObject.GetComponent<Image>().color = Color.green;
 				
 		});
 
+		/*
+		int FSM = 0;
 		gameObject.UpdateAsObservable ()
 			.Select (_ => Input.mousePosition)
 			.Where((pos)=> {
@@ -70,6 +58,8 @@ public class exam10_uirx_drag : MonoBehaviour {
 
 
 			});
+
+*/
 
 
 
