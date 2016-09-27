b9deba27af2bbb7db556de8f552a0f4a7d57cfac
도플광어 <gbox3d@gmail.com>
2016-09-27 14:19:46 +0900

    drag sample

--- ui_sample/Assets/exam10_uirx_drag/exam10_uirx_drag.cs
+++ ui_sample/Assets/exam10_uirx_drag/exam10_uirx_drag.cs
@@ -17,13 +17,52 @@ public class exam10_uirx_drag : MonoBehaviour {
 				Vector3 pos_tran = gameObject.GetComponent<RectTransform>().position;
 				return new Vector2(pos.x - pos_tran.x,pos.y - pos_tran.y);
 			})
-			.DistinctUntilChanged() // remove samethings
 			.Select(pos=>gameObject.GetComponent<RectTransform>().rect.Contains(pos))
+			.DistinctUntilChanged() // remove samethings
 			.Subscribe ((hover) => {
 				//Debug.Log( Mathf.RoundToInt( pos.x) + " , " +  Mathf.RoundToInt( pos.y) );
 				if(hover == true) gameObject.GetComponent<Image>().color = Color.red;
 				else gameObject.GetComponent<Image>().color = Color.green;
 		});
+
+
+		//drag 
+		Vector3 old_pos = Input.mousePosition;
+		gameObject.UpdateAsObservable ()
+			.Select (_ => Input.GetMouseButton (0))
+			.Where ((x) => {
+				if(x == false) {
+					old_pos = Input.mousePosition;
+				}
+				return x;
+			})
+			.Select (_ => {
+				Vector3 pos = Input.mousePosition;
+				Vector3 pos_tran = gameObject.GetComponent<RectTransform> ().position;
+				return new Vector2 (pos.x - pos_tran.x, pos.y - pos_tran.y);
+			})
+			.Where ((pos) => {
+				if(gameObject.GetComponent<RectTransform> ().rect.Contains (pos)) return true;
+				else {
+					old_pos = Input.mousePosition;;
+					return false;
+				}
+			})
+			.Select (_ => {
+				Vector2 rst_pos =  Input.mousePosition - old_pos;
+				old_pos = Input.mousePosition;
+				return rst_pos;
+			})
+			.DistinctUntilChanged ()
+			.Subscribe ((pos) => {
+				gameObject.GetComponent<RectTransform> ().localPosition += new Vector3(pos.x,pos.y,0);
+				Debug.Log(pos.ToString());
+			
+				
+		});
+		
+
+
 
 
 
