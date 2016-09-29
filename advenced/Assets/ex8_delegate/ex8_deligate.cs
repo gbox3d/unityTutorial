2ce9aeee20675ce37f8510c2dc04f05bb5d11cfa
도플광어 <gbox3d@gmail.com>
2016-09-29 20:42:46 +0900

    Merge branch 'patch-2' of https://github.com/acidsound/unityTutorial into acidsound-patch-2
    # Conflicts:
    #	ui_sample/Assets/exam10_uirx_drag/exam10_uirx_drag.cs

---
+++ advenced/Assets/ex8_delegate/ex8_deligate.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ex8_deligate : MonoBehaviour {
+
+
+	delegate void MyDelegateType();
+
+	Dictionary<string,MyDelegateType> dicMyDele;
+
+	void test_fun1()
+	{
+		Debug.Log ("hello dele");
+	}
+
+	void test_fun2()
+	{
+		Debug.Log ("hi dele");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		dicMyDele = new Dictionary<string,MyDelegateType> ();
+		MyDelegateType test1;
+		test1 = new MyDelegateType (test_fun1);
+
+		test1 ();
+
+		dicMyDele["callbackTest"] = new MyDelegateType (test_fun2);
+
+		//setup callback
+		MyDelegateType callBackTest;
+		if (dicMyDele.TryGetValue ("callbackTest", out callBackTest)) {
+			callBackTest ();
+			
+		} else {
+			Debug.Log ("not found");
+		}		
+
+		//remove it 
+		dicMyDele.Remove ("callbackTest");
+
+		if (dicMyDele.TryGetValue ("callbackTest", out callBackTest)) {
+			callBackTest ();
+
+		} else {
+			Debug.Log ("not found");
+		}	
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
