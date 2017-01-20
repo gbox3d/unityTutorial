bfe633a0092d81b1b4c69e34732c019bc9757909
도플광어 <gbox3d@gmail.com>
2017-01-20 10:49:27 +0900

    system example create

---
+++ system_Example/Assets/1.fileio_exam/3.directory/main.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Collections;
+using System.Collections.Generic;
+
+
+using UniRx;
+using UniRx.Triggers;
+
+using UnityEngine.UI;
+
+using LitJson;
+
+namespace com_advexam_system_fileio4
+{
+	
+	public class main : MonoBehaviour
+	{
+		[SerializeField] Text m_textLog;
+		[SerializeField] InputField m_inputName;
+
+		// Use this for initialization
+		void Start ()
+		{
+			transform.FindChild ("Button_getlist_file").GetComponent<Button> ().OnClickAsObservable ()
+				.Subscribe (_ => {
+					DirectoryInfo dir = new DirectoryInfo ( com_gunpower_system.myFileUtils.pathForDocumentsFile ("./"));
+					FileInfo[] info = dir.GetFiles ("*.*");
+
+					m_textLog.text = "";
+
+					foreach (FileInfo f in info) {
+						Debug.Log (f.Name + "," + f.Length + "," + f.DirectoryName + "," + f.FullName);
+						m_textLog.text += f.Name + "\n";
+
+						//file_list [f.Name] = f.Length;
+					}
+			});
+
+			transform.FindChild ("Button_getlist_dir").GetComponent<Button> ().OnClickAsObservable ()
+				.Subscribe (_ => {
+					DirectoryInfo dir = new DirectoryInfo ( com_gunpower_system.myFileUtils.pathForDocumentsFile ("./"));
+					DirectoryInfo[] info = dir.GetDirectories ();
+
+					m_textLog.text = "";
+					foreach (DirectoryInfo d in info) {
+						Debug.Log (d.Name + "," + d.FullName);
+						m_textLog.text += d.Name + "\n";
+						//file_list [f.Name] = f.Length;
+					}
+					
+			});
+
+			transform.FindChild ("Button_create_dir").GetComponent<Button> ().OnClickAsObservable ()
+				.Subscribe (_ => {
+					Directory.CreateDirectory ( com_gunpower_system.myFileUtils.pathForDocumentsFile (m_inputName.GetComponent<InputField>().text));
+					
+			});
+
+			transform.FindChild ("Button_del_dir").GetComponent<Button> ().OnClickAsObservable ()
+				.Subscribe (_ => {
+					Directory.Delete( com_gunpower_system.myFileUtils.pathForDocumentsFile (m_inputName.GetComponent<InputField>().text) );
+
+				});
+
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+	}
+
+}
---
+++ system_Example/Assets/2.udp_exam/udpex4.fastway/udpex4_fastway.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UniRx;
+//using UdpReceiverUniRx;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.Threading;
+
+//http://qiita.com/Napier_271828_/items/0d489c7d833047241079
+
+public class UdpState : System.IEquatable<UdpState>
+{
+	//UDP通信の情報を収める。送受信ともに使える
+	public IPEndPoint EndPoint {get; set;}
+	public string UdpMsg {get; set;}
+
+	public UdpState(IPEndPoint ep, string udpMsg)
+	{
+		this.EndPoint = ep;
+		this.UdpMsg = udpMsg;
+	}
+	public override int GetHashCode() {
+		return EndPoint.Address.GetHashCode();
+	}
+
+	public bool Equals(UdpState s)
+	{
+		if ( s == null ) {
+			return false;
+		}
+		return EndPoint.Address.Equals(s.EndPoint.Address);
+	}
+}
+
+
+public class udpex4_fastway : MonoBehaviour {
+
+	//public  UdpReceiverRx  _udpReceiverRx; 
+	//private  IObservable < UdpState >  _udpSequence ; 
+
+	private int listenPort = 1999;
+	private static UdpClient myClient;
+	private bool isAppQuitting;
+	public IObservable<UdpState> _udpSequence;
+
+	// Use this for initialization
+	void  Start ()  { 
+
+		isAppQuitting = false;
+
+		_udpSequence  =  Observable.Create < UdpState > ( observer  => 
+			{ 
+				Debug . Log ( string.Format ( "_udpSequence thread : {0}" ,  Thread.CurrentThread.ManagedThreadId )); 
+				try 
+				{ 
+					myClient  =  new  UdpClient ( listenPort ); 
+				} 
+				catch  ( SocketException  ex ) 
+				{ 
+					observer . OnError ( ex ); 
+				} 
+				IPEndPoint  remoteEP  =  null ; 
+				myClient . EnableBroadcast  =  true ; 
+				//myClient . Client . ReceiveTimeout  =  5000 ; 
+				while  (! isAppQuitting ) 
+				{ 
+					try 
+					{ 
+						remoteEP  =  null ; 
+						var  receivedMsg  =  System . Text . Encoding . ASCII . GetString ( myClient . Receive ( ref  remoteEP )) ; 
+						observer . OnNext ( new  UdpState ( remoteEP ,  receivedMsg )); 
+					} 
+					catch  ( SocketException ) 
+					{ 
+						Debug . Log ( "UDP :: Receive timeout" ); 
+					} 
+				} 
+				observer . OnCompleted (); 
+				return  null ; 
+			}) 
+			. SubscribeOn ( Scheduler . ThreadPool ) 
+			. Publish () 
+			. RefCount (); 
+
+		_udpSequence 
+			. ObserveOnMainThread () 
+			. Subscribe ( x  => { 
+				Debug.Log ( x . UdpMsg ); 
+				Debug.Log ( "receive remote ip :" + x . EndPoint.Address ); 
+				Debug.Log ( "receive remote port :" + x . EndPoint.Port ); 
+			}) 
+			. AddTo ( this ); 
+
+	} 
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnApplicationQuit()
+	{
+		isAppQuitting = true;
+		myClient.Client.Blocking = false;
+	}
+
+
+}
