c67d136f71d28e6de127562d1b2fadaeaa1618b0
도플광어 <gbox3d@gmail.com>
2017-01-20 20:02:48 +0900

    .....

---
+++ ui_sample/Assets/resources/gunpowerUI/file_dlg/FileDlg.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Collections;
+using System.Collections.Generic;
+
+
+using System.Threading;
+
+using UniRx;
+using UniRx.Triggers;
+
+using UnityEngine.UI;
+
+using LitJson;
+
+namespace com_gunpower_ui
+{
+
+	public class FileDlg : MonoBehaviour
+	{
+		[SerializeField] GameObject m_preFebListItem;
+		[SerializeField] GameObject m_btnOk;
+		[SerializeField] GameObject m_btnCancel;
+		[SerializeField] GameObject m_edtFilePath;
+		[SerializeField] GameObject m_fileListView;
+
+		//public delegate void cbdg_string();
+		public delegate void dgcb_string(string val);
+
+		public Subject<string> m_OnSelectOK;// = new Subject<string> ();
+		public Subject<string> m_OnCancel;// = new Subject<string> ();
+
+		GameObject m_SelectItem;
+
+		private bool m_reUse = true;
+
+		void refresh()
+		{
+
+			com_gunpower_ui.fileinfoItem[] items =  m_fileListView.GetComponentsInChildren<com_gunpower_ui.fileinfoItem> ();
+
+			for (int i = 0; i < items.Length; i++) {
+
+				GameObject node = items [i].gameObject;
+				node.transform.localPosition = new Vector2 ( 
+					0,0 - ((i) * node.GetComponent<RectTransform>().sizeDelta.y)
+				);
+
+			}
+		}
+
+		public void addItem(string strName,int nSize) {
+			
+			Transform node = GameObject.Instantiate(m_preFebListItem,m_fileListView.transform).transform;
+			//node.FindChild("Text").GetComponent<Text>().text = "test";
+
+			node.GetComponent<com_gunpower_ui.fileinfoItem> ().init (strName, nSize);
+			node.GetComponent<com_gunpower_ui.fileinfoItem> ().m_OnSelect.Subscribe (_ => {
+				m_SelectItem = node.gameObject;
+				Debug.Log("select : " +  m_SelectItem.GetComponent<com_gunpower_ui.fileinfoItem> ().m_fileinfo_strName);
+				m_edtFilePath.GetComponent<InputField>().text = m_SelectItem.GetComponent<com_gunpower_ui.fileinfoItem> ().m_fileinfo_strName;
+			}).AddTo(this);
+
+			refresh ();
+
+			m_fileListView.GetComponent<RectTransform>().sizeDelta = 
+				new Vector2(m_fileListView.GetComponent<RectTransform>().sizeDelta.x,
+					( node.GetComponent<RectTransform>().sizeDelta.y * m_fileListView.transform.childCount) );
+
+
+		}
+
+		void clearAllListItem() {
+
+			com_gunpower_ui.fileinfoItem[] items = m_fileListView.transform.GetComponentsInChildren<com_gunpower_ui.fileinfoItem> ();
+			m_fileListView.transform.DetachChildren ();
+
+			foreach (com_gunpower_ui.fileinfoItem item in items) {
+				item.remove ();
+			}
+		}
+
+		public void show(JsonData jsnDat,bool reUse=true,Subject<string> OnSelect=null, Subject<string> OnCencel=null)
+		{
+			if (OnSelect != null) {
+				m_OnSelectOK = OnSelect;
+			}
+
+			if (OnCencel != null ) {
+				m_OnCancel = OnCencel;
+			}
+
+			m_edtFilePath.GetComponent<InputField> ().text = "";
+			clearAllListItem ();
+			m_SelectItem = null;
+
+			foreach (string key in jsnDat.Keys) {
+
+				//Debug.Log (jsnDat [key]);
+				addItem (key,   int.Parse( jsnDat[key].ToString() ));
+			}
+			gameObject.SetActive (true);
+		}
+
+
+		public void close()
+		{
+			m_OnSelectOK.Dispose ();
+			m_OnCancel.Dispose ();
+
+
+
+			gameObject.SetActive(false);
+			if (!m_reUse) {
+				Destroy (gameObject);
+			}
+		}
+
+
+
+		// Use this for initialization
+		void Start ()
+		{
+
+			m_btnOk.GetComponent<Button> ().OnClickAsObservable ().Subscribe (_ => {
+
+				if(m_edtFilePath.GetComponent<InputField>().text.Length > 0) {
+					m_OnSelectOK.OnNext(
+						//m_SelectItem.GetComponent<com_gunpower_ui.fileinfoItem>().m_fileinfo_strName
+						m_edtFilePath.GetComponent<InputField>().text
+					);
+					close();
+				}
+			});
+
+			m_btnCancel.GetComponent<Button> ().OnClickAsObservable ().Subscribe (_ => {
+				//gameObject.SetActive(false);
+//				Debug.Log("click");
+				if(m_OnCancel != null) {
+					m_OnCancel.OnNext("");
+				}
+				close();
+
+			});
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+	}
+
+}
---
+++ ui_sample/Assets/resources/gunpowerUI/input_dlg/input_dlg.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+using UniRx;
+using UniRx.Triggers;
+
+using UnityEngine.UI;
+
+using LitJson;
+
+using System.Linq;
+
+namespace com_gunpower_ui
+{
+	
+	public class input_dlg : MonoBehaviour
+	{
+
+		[SerializeField]GameObject m_header;
+		[SerializeField]GameObject m_Input;
+
+		[SerializeField]Button m_btnOk;
+		[SerializeField]Button m_btnCancel;
+
+		public delegate void dg_CallBack (JsonData datJson);
+
+		dg_CallBack m_CallbackBtn;
+
+		bool m_reUse = false;
+
+		public static input_dlg createInstance(Transform parent)
+		{
+			GameObject prefeb = Resources.Load ("gunpowerUI/input_dlg/input_dlg",typeof(GameObject)) as GameObject;
+			GameObject dlgbox = GameObject.Instantiate (prefeb,parent) as GameObject;
+
+			return dlgbox.GetComponent<input_dlg>();
+		}
+
+		public void show (string title, string strVal, dg_CallBack callback,bool isReUse=false)
+		{
+			transform.localPosition = new Vector3 (0, 0, 0);
+			m_header.transform.FindChild ("Text").GetComponent<Text> ().text = title;
+			m_Input.GetComponent<InputField> ().text = strVal;
+			m_CallbackBtn = callback;
+			m_reUse = isReUse;
+			gameObject.SetActive (true);
+
+		}
+
+		public void close ()
+		{
+			//m_CallbackBtn();
+			gameObject.SetActive (false);
+			if (m_reUse == false) {
+				Destroy (gameObject, 0.5f);
+			}
+		
+		}
+
+		// Use this for initialization
+		void Start ()
+		{
+			//m_Input = transform.FindChild("body/InputField").gameObject;
+
+			m_btnOk.OnClickAsObservable ().Subscribe ((obj) => {
+
+				string strTemp = m_Input.transform.FindChild ("Text").GetComponent<Text> ().text;
+
+				if (strTemp.Length > 0) {
+					JsonData json_obj = new JsonData();
+					json_obj["ok"] = true;
+					json_obj["val"] = strTemp;
+					m_CallbackBtn (json_obj);
+					close ();
+				}
+			
+			});
+
+			m_btnCancel.OnClickAsObservable ().Subscribe ((obj) => {
+				JsonData json_obj = new JsonData();
+				json_obj["ok"] = false;
+				//json_obj["val"] = strTemp;
+				m_CallbackBtn (json_obj);
+				close ();
+
+			});
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+	}
+
+}
