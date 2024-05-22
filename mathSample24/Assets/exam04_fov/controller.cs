8ad2c8dd9e0286446fb23e7df6162029bb17373e
gbox3d <gbox3d@gmail.com>
2024-05-22 14:44:40 +0900

    ....

---
+++ mathSample24/Assets/exam04_fov/controller.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Camera viewCamera;
+    Vector3 velocity;
+    public float moveSpeed = 6;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+    // Update is called once per frame
+    void Update () {
+		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+		transform.LookAt (mousePos + Vector3.up * transform.position.y);
+		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
+	}
+	void FixedUpdate() {
+		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
